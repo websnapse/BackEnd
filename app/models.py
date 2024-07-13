2c07f453f857445b59256c390ca00c6b9419a3db
Jarred Luzada <jarred.luzada@maya.ph>
2024-07-13 17:29:26 +0800

    fix: added default value to optional parameter

--- app/models.py
+++ app/models.py
@@ -30,4 +30,4 @@ class Synapse(BaseModel):
 class SNPSystem(BaseModel):
     neurons: List[Union[Input, Output, Regular]]
     synapses: List[Synapse]
-    expected: Optional[List[object]]
+    expected: Optional[List[object]] = None
