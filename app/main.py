185b7669a338b93def790d3f32af24465868b41c
Jarred Luzada <jarred.luzada@maya.ph>
2024-07-13 23:27:09 +0800

    fix: added github websnapse to cors

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:5173",
+    "https://websnapse.github.io/",
 ]
 app.add_middleware(  # type: ignore
     CORSMiddleware,
